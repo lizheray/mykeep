b8487b5387298b6e9e9cfd1b6b20759db7494178
RayBlade <lizheray@gmail.com>
2017-02-28 12:24:21 +0800

    register

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,19 @@
 from flask_wtf import Form
 from wtforms import StringField, SubmitField, PasswordField, BooleanField
-from wtforms.validators import Required, Length, Email
+from wtforms.validators import Required, Length, Email, Regexp
+from wtforms import ValidationError
+from ..models import User
 
 class LoginForm(Form):
     email = StringField(validators=[Required(), Length(1, 64), Email()])
     password = PasswordField(validators=[Required()])
     remember_me = BooleanField()
     submit = SubmitField('Log in')
+
+class RegistrationForm(Form):
+    email = StringField(validators=[Required(), Length(1, 64), Email()])
+    username = StringField(validators=[Required(), Length(1, 64)])
+    password = PasswordField(validators=[Required()])
+    password2 = PasswordField(validators=[Required()])
+    submit = SubmitField('Register')
+
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
+import re
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user, login_required
 
 from . import auth
-from ..models import User
-from .forms import LoginForm
+from ..models import User, db
+from .forms import LoginForm, RegistrationForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -23,3 +24,24 @@ def logout():
     logout_user()
     return redirect(url_for('main.index'))
     
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    existEmail = False
+    existUsername = False
+    invalidUsername = False
+    dismatchpsd = False
+    if form.validate_on_submit():
+	if User.query.filter_by(email=form.email.data).first():
+	    existEmail = True
+	elif User.query.filter_by(username=form.username.data).first():
+	    existUsername = True
+	elif not re.match('^[A-Za-z][A-Za-z0-9_.]*$', form.username.data):
+	    invalidUsername = True
+	elif form.password2.data != form.password.data:
+	    dismatchpsd = True
+	else:
+	    user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+	    db.session.add(user)
+	    return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form, existEmail=existEmail, existUsername=existUsername, dismatchpsd=dismatchpsd, invalidUsername=invalidUsername)
